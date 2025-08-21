b3f82521da907fdd18faf5588944dd4230ea3fb2
ghost <ghostpy91.com>
2025-08-22 04:45:50 +0600

    rectangle area

---
+++ Day1/script.js
@@ -0,0 +1,12 @@
+// Rectangle Area
+
+function rectangleArea(length, width) {
+  const area = length * width;
+  if (area <= 0) {
+    console.log("Check the inputs!");
+  } else {
+    console.log("Area of the rectangle is : " + area);
+  }
+}
+
+rectangleArea(-10, 50);
